dfdde6a48a20e0644805120ef75be617ea857075
vikasthammannagowda <gowdavikas189@gmail.com>
2026-04-23 13:21:58 +0000

    Deploying to gh-pages from @ vikasthammannagowda/vikasthammannagowda.github.io@706eb87e85e818597516b4a350e4ff3d86903020 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://vikasthammannagowda.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://vtgowda.com/#contact">contact us</a>.'
               }
             ]
           }
